876e7a0986249b060d224f2564beae1650d5fb0a
webzth <utsav.ojha@hotmail.com>
2021-11-11 16:13:32 +0530

    Added winston logger for logging, added nodemailer and bull for email-queue feature

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
-import greet from './test';
-greet('Pardnurrrr');
+import http from 'http';
+import { Logger } from 'winston';
+import app from './app';
+import conf from './config';
+import { logger } from './utils';
+
+try {
+	http
+		.createServer(app)
+		.listen(
+			Number(conf.PORT),
+			conf.HOST,
+			(): Logger => logger.info(`🚀 server::running @ http://${conf.HOST}:${conf.PORT}`)
+		);
+} catch (error: any) {
+	logger.error(`🔺 ${error.name}::${error.message}`);
+}
